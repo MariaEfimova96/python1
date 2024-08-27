883381d7b0e233bbd6c3deac3dfd82d38ee50112
DNS <usatovamaria8@gmail.com>
2024-08-27 19:41:01 +0300

    Лекция 5

---
+++ LessonQA421.py
@@ -0,0 +1,13 @@
+'''Задание 1
+Выведите на экран надпись To be or not to be на разных строках'''
+# print(" To be \n or not \n to be")
+'''Задание 2
+Выведите на экран надпись «Life is what happens when you're busy making otherplans» John Lennon на разных строках. '''
+# print("Life is what happens \n\t when you're busy making \n\t\t other plans» John Lennon")
+'''Задание 3 
+Определите тип данных 10 и '10'''''
+# print(type('10'))
+# print(type(10))
+''' Задание 4
+Выведите на экран треугольник из звездочек'''
+# print('*\n**\n***\n****')
