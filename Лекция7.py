d0a5e26e330238943788e2d97c49a2df4f350fdb
DNS <usatovamaria8@gmail.com>
2024-09-01 16:34:58 +0300

    практика 1

---
+++ Лекция7.py
@@ -0,0 +1,23 @@
+# counter = 10
+# while counter != 7:
+#     print('Внутри цикла', counter)
+#     counter = counter - 1
+# print('Вне цикла', counter)
+#
+# counter = 27
+# while counter != 47:
+#     print('Внутри цикла', counter)
+#     counter = counter + 1
+# print('Вне цикла', counter)
+#
+# counter = 7
+# while counter != -2:
+#     print('Внутри цикла', counter)
+#     counter = counter + 2
+# print('Вне цикла', counter)
+#
+# counter = 5
+# while counter != -1:
+#     print('Внутри цикла', counter)
+#     counter = counter-5
+# print('Вне цикла', counter)
